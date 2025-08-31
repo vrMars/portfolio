afe8f1860cf713c5c7ac8a3b4b1e53d713a602b8
neelaksh bhatia <neelakshbhatia@neelakshs-MacBook-Pro.local>
2025-08-31 00:23:40 -0700

    hello world

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Set this to your repository name
-})
+  // IMPORTANT: Replace 'portfolio' with your GitHub repository name if it's different.
+  base: '/portfolio/',
+});
